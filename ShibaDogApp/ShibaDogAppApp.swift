6e2011152eb3b69f56b14e2b6da3a57c357b4c1b
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-04-02 23:36:27 +0900

    Initial Commit

---
+++ ShibaDogApp/ShibaDogAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShibaDogAppApp.swift
+//  ShibaDogApp
+//
+//  Created by 渡邊魁優 on 2023/04/02.
+//
+
+import SwiftUI
+
+@main
+struct ShibaDogAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
